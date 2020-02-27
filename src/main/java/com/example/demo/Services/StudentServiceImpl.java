be4968943e6a480658fe08c04d4fbaaeb2caf3c6
Jmokut <joshuamokut@gmail.com>
2020-02-27 20:09:52 +0300

    Unnecessary import removed

--- src/main/java/com/example/demo/Services/StudentServiceImpl.java
+++ src/main/java/com/example/demo/Services/StudentServiceImpl.java
@@ -1,7 +1,6 @@
 package com.example.demo.Services;
 
 import com.example.demo.Entities.Student;
-import com.example.demo.Respositories.GroupRepository;
 import com.example.demo.Respositories.StudentRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
