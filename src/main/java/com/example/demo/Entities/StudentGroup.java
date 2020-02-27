2af0c1947580c5245cb46202fc10a617ca66f6fd
Jmokut <joshuamokut@gmail.com>
2020-02-27 20:15:07 +0300

    Introduced lombok data, and constructor annotation to reduce code size

--- src/main/java/com/example/demo/Entities/StudentGroup.java
+++ src/main/java/com/example/demo/Entities/StudentGroup.java
@@ -1,52 +1,23 @@
 package com.example.demo.Entities;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Getter
+@Data
+@RequiredArgsConstructor
 @Entity
 public class StudentGroup {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String name;
-    private String description;
+    @NonNull private String name;
+    @NonNull private String description;
 
     protected StudentGroup(){}
 
-    public StudentGroup(String name, String description){
-        this.name=name;
-        this.description=description;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
