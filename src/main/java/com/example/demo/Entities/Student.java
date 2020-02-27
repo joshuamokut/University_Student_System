7521871faf531ab67458b146d489f1e6bceec481
Jmokut <joshuamokut@gmail.com>
2020-02-27 20:17:17 +0300

    Introduced lombok data annotation to reduce code size

--- src/main/java/com/example/demo/Entities/Student.java
+++ src/main/java/com/example/demo/Entities/Student.java
@@ -18,6 +18,7 @@ import java.time.LocalDate;
 
 
 @Data
+
 @Entity
 public class Student {
 
@@ -40,51 +41,5 @@ public class Student {
         this.groupId=groupId;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
-    }
-
-    public void setGroupId(Long groupId) {
-        this.groupId = groupId;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public Long getGroupId() {
-        return groupId;
-    }
 }
