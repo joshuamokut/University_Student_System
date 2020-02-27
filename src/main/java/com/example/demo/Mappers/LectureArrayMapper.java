17b526151a94c60b6a2cdad31d9db5c3b5ec1a36
Jmokut <joshuamokut@gmail.com>
2020-02-27 20:32:12 +0300

    Created LectureArrayMapper

---
+++ src/main/java/com/example/demo/Mappers/LectureArrayMapper.java
@@ -0,0 +1,22 @@
+package com.example.demo.Mappers;
+
+import com.example.demo.DTO.LectureDTO;
+import com.example.demo.Entities.Lecture;
+import lombok.AllArgsConstructor;
+
+import java.util.ArrayList;
+
+@AllArgsConstructor
+public class LectureArrayMapper {
+    private final LectureMapper lectureMapper;
+
+    public ArrayList<LectureDTO>MapLecturesToArray(ArrayList<Lecture> lectures){
+        ArrayList<LectureDTO> lectureDTOS=new ArrayList<>();
+
+        for(Lecture lecture: lectures){
+            lectureDTOS.add(lectureMapper.MapLecture(lecture));
+        }
+
+        return lectureDTOS;
+    }
+}
