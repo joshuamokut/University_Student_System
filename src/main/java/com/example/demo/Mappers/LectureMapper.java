27fecda3e5e74d099901f637dadbf5580d11b832
Jmokut <joshuamokut@gmail.com>
2020-02-28 18:43:11 +0300

    Replaced ineffective numerous calls to database

--- src/main/java/com/example/demo/Mappers/LectureMapper.java
+++ src/main/java/com/example/demo/Mappers/LectureMapper.java
@@ -1,7 +1,10 @@
 package com.example.demo.Mappers;
 
 import com.example.demo.DTO.LectureDTO;
+import com.example.demo.DTO.StudentDTO;
 import com.example.demo.Entities.Lecture;
+import com.example.demo.Entities.Student;
+import com.example.demo.Entities.StudentGroup;
 import com.example.demo.Respositories.GroupRepository;
 import com.example.demo.Respositories.StudentRepository;
 import com.example.demo.Utilities.CustomDuration;
@@ -9,22 +12,32 @@ import com.example.demo.Utilities.CustomTime;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @AllArgsConstructor
 @Component
 public class LectureMapper {
 
     private final StudentRepository studentRepository;
     private final GroupRepository groupRepository;
-    private final StudentArrayMapper studentArrayMapper;
+    private final StudentMapper studentMapper;
+    private List<StudentDTO> studentDTOList;
 
+    public LectureDTO mapLecture(Lecture lecture){
+        studentDTOList.clear();
 
-    public LectureDTO MapLecture(Lecture lecture){
+        for(StudentGroup group: lecture.getStudentGroups()){
+            for(Student student: group.getStudents()){
+                studentDTOList.add(studentMapper.MapStudent(student));
+            }
+        }
+
         return new LectureDTO(new CustomDuration(lecture.getStartDate().atTime(lecture.getStartTime()),
                 lecture.getEndDate().atTime(lecture.getEndTime())),
                 lecture.getVenue(),
                 new CustomTime(lecture.getStartDate().atTime(lecture.getStartTime())),
                 new CustomTime(lecture.getEndDate().atTime(lecture.getEndTime())),
-                studentArrayMapper.MapStudentsToArray(studentRepository.findAllByGroupIdEquals(groupRepository.findFirstByName(lecture.getGroupName()).getId()))
+                studentDTOList
                 );
     }
 }
