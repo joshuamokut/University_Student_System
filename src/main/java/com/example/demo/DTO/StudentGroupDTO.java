f27a6b9fa89d894f6c27f8e44d9cfa62186f696d
Jmokut <joshuamokut@gmail.com>
2020-03-02 21:14:33 +0300

    Included @Data annotation Lombok. Serializer wasn't working because there were no getters

--- src/main/java/com/example/demo/DTO/StudentGroupDTO.java
+++ src/main/java/com/example/demo/DTO/StudentGroupDTO.java
@@ -1,10 +1,12 @@
 package com.example.demo.DTO;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 import java.util.List;
 
 @AllArgsConstructor
+@Data
 public class StudentGroupDTO {
     private final String name;
     private final String description;
