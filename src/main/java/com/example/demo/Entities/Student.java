87bde450b8a31a5442b98265796b8c89f0302060
Jmokut <joshuamokut@gmail.com>
2020-03-02 21:17:47 +0300

    Foreign key to student's group.

--- src/main/java/com/example/demo/Entities/Student.java
+++ src/main/java/com/example/demo/Entities/Student.java
@@ -1,22 +1,14 @@
 package com.example.demo.Entities;
 
 import com.example.demo.Utilities.Gender;
-import javafx.scene.Group;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import org.apache.tomcat.jni.Local;
 
 import javax.persistence.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.time.LocalDate;
 
 
 @Data
-
 @Entity
 public class Student {
 
@@ -27,16 +19,20 @@ public class Student {
     private String surname;
     private LocalDate dateOfBirth;
     private Gender gender;
-    @OneToOne private StudentGroup group;
+    private String studentGroupName;
+
+
+    @ManyToOne
+    private StudentGroup studentGroup;
 
     protected Student(){}
 
-    public Student(String name, String surname, String dateOfBirth, Gender gender, StudentGroup group) {
+    public Student(String name, String surname, String dateOfBirth, Gender gender, String studentGroupName) {
         this.name=name;
         this.surname=surname;
         this.dateOfBirth=LocalDate.parse(dateOfBirth,  DateTimeFormatter.ofPattern("dd/MM/yyyy"));
         this.gender=gender;
-        this.group=group;
+        this.studentGroupName=studentGroupName;
     }
 
 
